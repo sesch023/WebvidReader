c74597a820c883c636697c6e8764a3dbeef817be
Sebastian Schmidt <schmidt.sebastian2@fh-swf.de>
2023-05-08 13:24:40 +0000

    Fixed: Error with unstacked tensor, Changed: Now frame by frame reading is only attenpted if batch reading fails to optimize the reading process

--- WebvidReader/Video.py
+++ WebvidReader/Video.py
@@ -25,21 +25,25 @@ def read_video_object(video, start=0, end=None, channels_first=False, broken_fra
             f"start time={start} and end time={end}"
         )
 
-    video_frames = []
+    try:
+        video_frames = video.get_batch(range(min(len(video), end)))
+    except Exception as e:
+        print(f"Warning: Failed read video as batch, going to frame by frame reading. The cause was: {str(e)}")
+        video_frames = []
+        video.seek(0)
+        for i in range(min(len(video), end)):
+            try:
+                video_frames.append(video.next())   
+            except Exception as e:
+                if(broken_frame_warning_only):
+                    print(f"Warning: Failed to load frame '{i}' of video, frame will be skipped. The cause was: {str(e)}")
+                else:
+                    raise RuntimeError(f"Error: Failed to load frame '{i}' of video. The cause was: {str(e)}")
     
-    for i in range(min(len(video), end)):
-        try:
-            video_frames.append(video[i])
-        except Exception as e:
-            if(broken_frame_warning_only):
-                print(f"Warning: Failed to load frame '{i}' of video, frame will be skipped. The cause was: {str(e)}")
-            else:
-                raise RuntimeError(f"Warning: Failed to load frame '{i}' of video. The cause was: {str(e)}")
-            
+        video_frames = torch.stack(video_frames)
     
-    video_frames = torch.Tensor(video_frames)
     if channels_first and len(video_frames.shape) == 4:
         video_frames = video_frames.permute(0, 3, 2, 1)
-        
+    
     return video_frames
 
