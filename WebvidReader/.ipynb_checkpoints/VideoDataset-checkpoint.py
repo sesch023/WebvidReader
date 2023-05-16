Fixed: Normalization

WebvidReader/.ipynb_checkpoints/VideoDataset-checkpoint.py
@@ -8,7 +8,6 @@ import pickle
 import os
 from tqdm import tqdm
 import time
-from torch.nn.functional import normalize
 
 VideoItem = namedtuple("VideoItem", ["Caption", "Path"])
 
@@ -65,6 +64,12 @@ class VideoDataset(Dataset):
     def __old_equals_self__(self, old):
         return (self._channels_first == old._channels_first and self._target_resolution == old._target_resolution and self._csv_path == old._csv_path and self._video_base_path == old._video_base_path and self._max_frames_per_part == old._max_frames_per_part and self._first_frame_only == old._first_frame_only and self._min_frames_per_part == old._min_frames_per_part and self._normalize == old._normalize)
 
+    def __normalize_frame__(self, frame):
+        return torch.add(torch.div(frame, 255/2), -1) 
+    
+    def __normalize_to_image(self, video):
+        return torch.mul(torch.add(video, 1), 255/2)
+    
     def __len__(self):
         return len(self._video_map)
 
@@ -105,27 +110,35 @@ class VideoDataset(Dataset):
         if video is not None:
             if self._first_frame_only:
                 video = torch.Tensor(video).float()
+                if self._normalize:
+                    video = self.__normalize_frame__(video)
             else:
                 for i in range(len(video)):
                     video[i] = torch.Tensor(video[i]).float() if video[i] is not None else None
-            if self._normalize:
-                video = torch.add(torch.mul(normalize(video, p=1), 2), -1)
+                    if video[i] is not None and self._normalize:
+                        video[i] = self.__normalize_frame__(video[i])
             
         return video, label
     
     
     def write_item(self, idx, path):
         video, label = self.__getitem__(idx)
+        
+        if video is None or (isinstance(video, list) and any(map(lambda x: x is None, video))):
+            print("Warning: Cannot write video with missing parts.")
+        
         target_resolution= self._crop_frames if self._crop_frames is not None else self._target_resolution
         
-        if self._normalize:
-            video = torch.mul(torch.add(video, 1), 255/2)
-        
         if not isinstance(video, list):
+            if self._normalize:
+                video = self.__normalize_to_image(video)
+            
             write_video_object(path, video, channels_first=self._channels_first, target_resolution=target_resolution)
         else:
             path_f = path[:-4] + "_{num}" + path[-4:]
             for i in range(len(video)):
+                if video[i] is not None and self._normalize:
+                    video[i] = self.__normalize_to_image(video[i])
                 path_c = path_f.format(num=i)
                 write_video_object(path_c, video[i], channels_first=self._channels_first, target_resolution=target_resolution)
         