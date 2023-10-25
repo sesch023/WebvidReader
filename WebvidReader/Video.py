5682ef73423564b3e4eee2927ee7bc1c49876eb6
Sebastian Schmidt <schmidt.sebastian2@fh-swf.de>
2023-10-25 20:28:16 +0000

    Added: Doku

--- WebvidReader/Video.py
+++ WebvidReader/Video.py
@@ -12,12 +12,25 @@ from einops import rearrange
 decord.bridge.set_bridge('torch')
 
 class IllegalStartException(Exception):
+    """
+    Raised when the start frame is larger than the video length.
+    """    
     pass
 
 class IllegalFrameException(Exception):
+    """
+    Raised when a frame cannot be read.
+    """    
     pass
 
 def _get_video_reader(path, target_resolution):
+    """
+    Get a video reader for the given path and target resolution.
+
+    :param path: Path to the video file.
+    :param target_resolution: The target resolution of the video.
+    :return: A video reader for the given path and target resolution.
+    """    
     if target_resolution is not None:
         video = VideoReader(path, ctx=cpu(1), width=target_resolution[0], height=target_resolution[1])
     else:
@@ -26,11 +39,39 @@ def _get_video_reader(path, target_resolution):
     return video
 
 def read_video_file(path, start=0, end=None, target_ordering="c t h w", target_resolution=(426, 240), crop_frames=None, nth_frames = 1, broken_frame_warning_only=True):
+    """
+    Read a video file as a tuple containing the video frames and the fps of the video.
+
+    :param path: Path to the video file.
+    :param start: Start frame, defaults to 0
+    :param end: End frame, defaults to None which means the end of the video.
+    :param target_ordering: The order of the dimensions of the returned tensor, defaults to "c t h w"
+    :param target_resolution: The target resolution of the video, defaults to (426, 240)
+    :param crop_frames: Crop the frames to the given size, defaults to None
+    :param nth_frames: Only read every nth frame, defaults to 1
+    :param broken_frame_warning_only: If true, only print a warning when a frame cannot be read, defaults to True
+    :return: A tuple containing the video frames and the fps of the video.
+    """    
     video = _get_video_reader(path, target_resolution)
     return read_video_object(video, start, end, target_ordering, crop_frames, nth_frames, broken_frame_warning_only)
 
 
 def read_video_object(video, start=0, end=None, target_ordering="c t h w", crop_frames=None, nth_frames = 1, broken_frame_warning_only=True):
+    """
+    Read a video object as a tuple containing the video frames and the fps of the video.
+
+    :param video: The video object.
+    :param start: Start frame, defaults to 0
+    :param end: End frame, defaults to None which means the end of the video.
+    :param target_ordering: The order of the dimensions of the returned tensor, defaults to "c t h w"
+    :param crop_frames: Crop the frames to the given size, defaults to None
+    :param nth_frames: Only read every nth frame, defaults to 1
+    :param broken_frame_warning_only: If true, only print a warning when a frame cannot be read, defaults to True
+    :raises ValueError: If the end frame is smaller than the start frame.
+    :raises IllegalStartException: If the start frame is larger than the video length.
+    :raises IllegalFrameException: If a frame cannot be read.
+    :return: A tuple containing the video frames and the fps of the video.
+    """    
     # End is excluded
     if end is None:
         end = float("inf")
@@ -40,20 +81,27 @@ def read_video_object(video, start=0, end=None, target_ordering="c t h w", crop_
             f"start time={start} and end time={end}"
         )
     
+    # Get the video fps
     fps = video.get_avg_fps()
+    # Get the frames to take
     frames_taken = range(start, min(len(video), end), nth_frames)
+    # Try to read the video as a batch
     try:
         video_frames = video.get_batch(frames_taken)
     except Exception as e:
+        # If it fails, read frame by frame
         print(f"Warning: Failed read video as batch, going to frame by frame reading. The cause was: {str(e)}")
         video_frames = []
         current_frame = 0
         frames_taken = list(frames_taken)
         try:
+            # Seek to the start frame
             video.seek(start)
         except Exception as e:
             raise IllegalStartException(f"Error: Failed to seek start frame '{start}' of video. The video is likely shorter than '{start}' frames. The cause was: {str(e)}")
+        # Try to read the frames, frame by frame
         for i in range(min(len(video), end)):
+            # Read the next frame
             try:
                 if current_frame in frames_taken:
                     video_frames.append(video.next())   
@@ -64,8 +112,10 @@ def read_video_object(video, start=0, end=None, target_ordering="c t h w", crop_
                 else:
                     raise IllegalFrameException(f"Error: Failed to load frame '{i}' of video. The cause was: {str(e)}")
         
+        # Convert the frames to a tensor
         video_frames = torch.stack(video_frames)
-        
+    
+    # Crop the frames if needed
     if crop_frames is not None:
         video_frames = video_frames.permute(0, 3, 2, 1)
         transform = transforms.CenterCrop(crop_frames)
@@ -75,26 +125,52 @@ def read_video_object(video, start=0, end=None, target_ordering="c t h w", crop_
     if len(video_frames.shape) == 3:
         video_frames = video_frames.unsqueeze(0)
 
+    # Reorder the dimensions
     video_frames = rearrange(video_frames, f"t h w c -> {target_ordering}")
     return video_frames, fps
 
 
 def read_video_file_as_parts(path, frames_per_part, target_ordering="c t h w", target_resolution=(426, 240), crop_frames=None, nth_frames = 1, broken_frame_warning_only=True):
+    """
+    Read a video file as a list of tuples containing multiple part of video frames and the fps of each part.
+
+    :param path: Path to the video file.
+    :param frames_per_part: The number of frames per part.
+    :param target_ordering: The order of the dimensions of the returned tensor, defaults to "c t h w"
+    :param target_resolution: The target resolution of the video, defaults to (426, 240)
+    :param crop_frames: Crop the frames to the given size, defaults to None
+    :param nth_frames: Only read every nth frame, defaults to 1
+    :param broken_frame_warning_only: If true, only print a warning when a frame cannot be read, defaults to True
+    :return: A list of tuples containing multiple part of video frames and the fps of each part.
+    """    
     video = _get_video_reader(path, target_resolution)
     return read_video_object_as_parts(video, frames_per_part, target_ordering, crop_frames, nth_frames, broken_frame_warning_only)
 
 
 def read_video_object_as_parts(video, frames_per_part, target_ordering="c t h w", crop_frames=None, nth_frames = 1, broken_frame_warning_only=True):
+    """
+    Read a video object as a list of tuples containing multiple part of video frames and the fps of each part.
+
+    :param video: The video object.
+    :param frames_per_part: The number of frames per part.
+    :param target_ordering: The order of the dimensions of the returned tensor, defaults to "c t h w"
+    :param crop_frames: Crop the frames to the given size, defaults to None
+    :param nth_frames: Only read every nth frame, defaults to 1
+    :param broken_frame_warning_only: If true, only print a warning when a frame cannot be read, defaults to True
+    :return: A list of tuples containing multiple part of video frames and the fps of each part.
+    """    
     start = 0
     end = frames_per_part
     frames = []
     frame_fps = []
     
+    # Read the video in parts, as long as the video part is longer than the frames per part
     while(True):
         frame, fps = read_video_object(video, start, end, target_ordering, crop_frames, nth_frames, broken_frame_warning_only)
         frames.append(frame)  
         frame_fps.append(fps)           
         
+        # Break if the video is shorter than the frames per part
         if frames_per_part is None or frame.shape[0] < frames_per_part:
             break
             
@@ -105,6 +181,16 @@ def read_video_object_as_parts(video, frames_per_part, target_ordering="c t h w"
 
 
 def write_video_object(path, torch_data, video_ordering="c t h w", target_resolution=None, fps=25, writing_codec='mp4v'):
+    """
+    Write a video object to a video file using OpenCV.
+
+    :param path: Path to the video file.
+    :param torch_data: The video object as a tensor.
+    :param video_ordering: The order of the dimensions of the tensor, defaults to "c t h w"
+    :param target_resolution: The target resolution of the written video, defaults to None which means the resolution of the tensor.
+    :param fps: The fps of the written video, defaults to 25
+    :param writing_codec: The codec used to write the video, defaults to 'mp4v'
+    """    
     torch_data = rearrange(torch_data, f"{video_ordering} -> t h w c")
     
     if target_resolution is None:
--- WebvidReader/VideoDataset.py
+++ WebvidReader/VideoDataset.py
@@ -10,12 +10,20 @@ from tqdm import tqdm
 import time
 from einops import rearrange
 
+# A named tuple to store the path and the caption of a video.
 VideoItem = namedtuple("VideoItem", ["Caption", "Path"])
 
 class VideoDataset(Dataset):
-    
     @staticmethod
-    def parse_csv(csv_path, pickle_vid_data=False, pickle_base_path=".video_pickles", verbose=False):
+    def parse_csv(csv_path, verbose=False):
+        """
+        Parses the csv file and returns a dictionary of VideoItems and a list of keys.
+        The csv file is in the format given by the WebVid-10M dataset.
+
+        :param csv_path: Path to the csv file.
+        :param verbose: If true, a progress bar will be shown.
+        :return: A tuple of a dictionary of VideoItems and a list of keys with video ids.
+        """        
         items = dict()
         csv = pandas.read_csv(csv_path)
         keys = list(csv["videoid"])
@@ -32,6 +40,27 @@ class VideoDataset(Dataset):
         return items, keys
 
     def __init__(self, csv_path, video_base_path, target_ordering="c t h w", target_resolution=(426, 240), crop_frames=None, pickle_vid_data=False, pickle_base_path="video_pickles", verbose=True, first_part_only=False, max_frames_per_part=float("inf"), min_frames_per_part=0, nth_frames=1, normalize=True):
+        """
+        Creates a new VideoDataset.
+
+        :param csv_path: Path to the csv file.
+        :param video_base_path: Path to the directory containing the videos.
+        :param target_ordering: The ordering of the video dimensions. The default is "c t h w", 
+                                which means that the video has the dimensions (channels, time, height, width).
+        :param target_resolution: The target resolution of the video. The default is (426, 240).
+        :param crop_frames: If not None, the video will be cropped to the given resolution.
+        :param pickle_vid_data: If true, the video data will be pickled to the given pickle_base_path. 
+                                This can be used to speed up the loading of the dataset, but it requires
+                                huge amounts of disk space.
+        :param pickle_base_path: The base path for the pickled video data.
+        :param verbose: If true, a progress bar will be shown when parsing the csv file.
+        :param first_part_only: If true, only the first part of the video defined by max and min frames per part will be loaded, 
+                                all frames after the first part will be discarded.
+        :param max_frames_per_part: The maximum number of frames per part.
+        :param min_frames_per_part: The minimum number of frames per part.
+        :param nth_frames: Only every nth frame will be loaded.
+        :param normalize: If true, the video data will be normalized to the range [-1, 1].
+        """        
         self._csv_path = csv_path
         self._video_base_path = video_base_path
         self._target_ordering = target_ordering
@@ -50,46 +79,96 @@ class VideoDataset(Dataset):
         if pickle_vid_data and not os.path.exists(pickle_base_path):
             os.makedirs(pickle_base_path)
         
-        self._video_map, self._keys = self.parse_csv(csv_path, pickle_vid_data, pickle_base_path, verbose=verbose)
+        self._video_map, self._keys = self.parse_csv(csv_path, verbose=verbose)
         self._repickle = False
         
+        # Check if the pickle file exists and check if the dataset has changed.
         if pickle_vid_data:
             dataset_pickle = f"{pickle_base_path}/dataset.bin"
             if os.path.isfile(dataset_pickle):
+                # Load the old dataset.
                 with open(dataset_pickle, "rb") as f:
                     old = pickle.load(f)
+                # Check if the dataset has changed. If yes set the repickle flag.
                 if not(self.__old_equals_self__(old)):
                     self._repickle = True
             with open(dataset_pickle, "wb") as f:
                 pickle.dump(self, f)
     
     def __old_equals_self__(self, old):
+        """
+        Checks if all the relevant values in the given VideoDataset are equal to the value in this VideoDataset.
+
+        :param old: The VideoDataset to compare to.
+        :return: True if all relevant values are equal, False otherwise.
+        """        
         return (self._target_ordering == old._target_ordering and self._target_resolution == old._target_resolution and self._csv_path == old._csv_path and self._video_base_path == old._video_base_path and self._max_frames_per_part == old._max_frames_per_part and self._first_part_only == old._first_part_only and self._min_frames_per_part == old._min_frames_per_part and self._normalize == old._normalize)
 
     def normalize(self, data):
+        """
+        Normalizes the given data from [0, 255] to the range [-1, 1].
+        Since the package was so far only used for diffusion, this was hardcoded.
+
+        :param data: The data to normalize.
+        :return: The normalized data.
+        """        
         return VideoDataset.normalize(data)
     
     def reverse_normalize(self, data):
+        """
+        Reverses the normalization of the given data from the range [-1, 1] to [0, 255].
+        Since the package was so far only used for diffusion, this was hardcoded.
+
+        :param data: The data to reverse normalize.
+        :return: The reverse normalized data.
+        """        
         return VideoDataset.reverse_normalize(data)
     
     @staticmethod
     def normalize(data):
+        """
+        Normalizes the given data from [0, 255] to the range [-1, 1].
+        Since the package was so far only used for diffusion, this was hardcoded.
+
+        :param data: The data to normalize.
+        :return: The normalized data.
+        """        
         return torch.add(torch.div(data, 255/2), -1) 
     
     @staticmethod
     def reverse_normalize(data):
+        """
+        Reverses the normalization of the given data from the range [-1, 1] to [0, 255].
+
+        :param data: The data to reverse normalize.
+        :return: The reverse normalized data.
+        """        
         return torch.mul(torch.add(data, 1), 255/2)
     
     def __len__(self):
+        """
+        Returns the number of videos in the dataset.
+
+        :return: The number of videos in the dataset.
+        """        
         return len(self._video_map)
 
     def __getitem__(self, idx):
+        """
+        Returns the video at the given index.
+
+        :param idx: The index of the video to return.
+        :return: A tuple of the video, the label and the fps of the video.
+        """        
         key = self._keys[idx]
         video_meta = self._video_map[key]
+        # Check if the video is already pickled and if it should be loaded from the pickle.
         pickle_path = f"{self._pickle_base_path}/{video_meta.Path.replace('/', '')}.nbz" if self._pickle_vid_data else None
+        # Check if the pickle file exists and if it should be loaded.
         load_pickle = self._pickle_vid_data and os.path.isfile(pickle_path) and not self._repickle
         fps = None
         
+        # Try to load the video from the pickle file.
         if load_pickle:
             try:
                 with open(pickle_path, "rb") as f:
@@ -98,18 +177,21 @@ class VideoDataset(Dataset):
                 print(f"Warning: Failed to load numpy pickle '{pickle_path}', falling back to reading the according video file. The cause was: {str(e)}")
                 load_pickle = False
         
+        # If the video was not loaded from the pickle file, load it from the video file.
         if not load_pickle:
             vid_path = f"{self._video_base_path}/{video_meta.Path}"
             
             try:
+                # If only the first part of the video should be loaded, load the video as a whole with the given range.
                 if self._first_part_only:
                     video, fps = read_video_file(vid_path, target_ordering=self._target_ordering, target_resolution=self._target_resolution, end=self._max_frames_per_part, crop_frames=self._crop_frames, nth_frames=self._nth_frames)
                     video = None if video.shape[self._time_dim] < self._min_frames_per_part else video
+                # If the whole video should be loaded, load the video as parts with the given range.
                 else:
                     video, fps = read_video_file_as_parts(vid_path, self._max_frames_per_part, target_ordering=self._target_ordering, target_resolution=self._target_resolution, crop_frames=self._crop_frames, nth_frames=self._nth_frames)
                     video = list(filter(lambda el: el.shape[self._time_dim] >= self._min_frames_per_part, video)) if self._min_frames_per_part > 0 else video
                 
-                
+                # If the video should be pickled, pickle it.
                 if self._pickle_vid_data:
                     with open(pickle_path, "wb") as f:
                         pickle.dump((video, fps), f)
@@ -117,12 +199,15 @@ class VideoDataset(Dataset):
                 print(f"Warning: Failed to load MP4 '{vid_path}', the Data will be returned as None. The cause was: {str(e)}")
                 video = None
         
+        # If the video is not None, convert it to a tensor and normalize it.
         label = video_meta.Caption
         if video is not None:
+            # If only the first part of the video should be loaded, convert the video to a tensor.
             if self._first_part_only:
                 video = torch.Tensor(video).float()
                 if self._normalize:
                     video = self.normalize(video)
+            # If the whole video should be loaded, convert the video parts to tensors.
             else:
                 for i in range(len(video)):
                     video[i] = torch.Tensor(video[i]).float() if video[i] is not None else None
@@ -132,20 +217,34 @@ class VideoDataset(Dataset):
         return video, label, fps
     
     def write_item(self, idx, path):
+        """
+        Writes the video at the given index to the given path.
+
+        :param idx: The index of the video to write.
+        :param path: The path to write the video to.
+        """        
         video, label = self.__getitem__(idx)
         self.write_video(video, path)
 
     def write_video(self, video, path):
+        """
+        Writes the given video to the given path.
+
+        :param video: The video to write.
+        :param path: The path to write the video to.
+        """        
         if video is None or (isinstance(video, list) and any(map(lambda x: x is None, video))):
             print("Warning: Cannot write video with missing parts.")
         
         target_resolution= self._crop_frames if self._crop_frames is not None else self._target_resolution
         
+        # If the video is not a list, it is a single video.
         if not isinstance(video, list):
             if self._normalize:
                 video = self.reverse_normalize(video)
             
             write_video_object(path, video, video_ordering=self._target_ordering, target_resolution=target_resolution)
+        # If the video is a list, it is a list of parts.
         else:
             path_f = path[:-4] + "_{num}" + path[-4:]
             for i in range(len(video)):
