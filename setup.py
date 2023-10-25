0b6ae6665b0562ce9c8afd4bc1eb9f679c3c14eb
Sebastian Schmidt <schmidt.sebastian2@fh-swf.de>
2023-10-25 20:31:27 +0000

    Set classifiers

--- setup.py
+++ setup.py
@@ -21,12 +21,6 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.7+',
     ],
 )
